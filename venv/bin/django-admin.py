37d877db7595ce1e1305523645deed788f4d6064
xiaomu <stephen@Stephen-Moo.local>
2020-02-22 12:30:44 +0800

    [INIT]项目初始化

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/stephen/Documents/个人/绝世秘籍/django/本书源码v1.1/第11章/music/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
